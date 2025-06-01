a70ec88525bb33db6083f8b392574f817ed2f6b5
Cristian Camilo Vasquez Rincón <carincon93@gmail.com>
2025-05-31 23:15:37 -0500

    fix: restore base path in Vite configuration to '/daily-tasks-app'

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "",
-
+  base: "/daily-tasks-app",
+  
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
