1a79e49b8e2671d2b995ab18e7ff83a19711e3a8
Cristian Camilo Vasquez Rincón <carincon93@gmail.com>
2025-06-24 20:49:31 -0500

    fix: remove unused background color assignment function and simplify task rendering

--- src/components/tasks/tasks.tsx
+++ src/components/tasks/tasks.tsx
@@ -303,28 +303,6 @@ function Tasks() {
     });
   };
 
-  const assignTaskBackgroundColor = (index: number): string => {
-    const colors = [
-      "bg-red-100",
-      "bg-blue-100",
-      "bg-green-100",
-      "bg-yellow-100",
-      "bg-purple-100",
-      "bg-pink-100",
-      "bg-orange-100",
-      "bg-indigo-100",
-      "bg-gray-100",
-      "bg-teal-100",
-      "bg-lime-100",
-      "bg-cyan-100",
-      "bg-amber-100",
-      "bg-fuchsia-100",
-      "bg-rose-100",
-    ];
-
-    return colors[index % colors.length];
-  };
-
   useEffect(() => {
     if (!taskSelectedToUpdate) return;
 
@@ -414,7 +392,7 @@ function Tasks() {
               tasks
                 .filter((task) => task.is_visible)
                 .sort((a, b) => b.milliseconds - a.milliseconds)
-                .map((task, index) => (
+                .map((task) => (
                   <li
                     key={task.id}
                     className={`relative text-black mb-4 rounded-lg ${
@@ -424,7 +402,7 @@ function Tasks() {
                       backgroundColor: task.category.color
                         ? `${task.category.color.replace(
                             /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i,
-                            (m, r, g, b) =>
+                            (r, g, b) =>
                               `rgba(${parseInt(r, 16)},${parseInt(
                                 g,
                                 16
